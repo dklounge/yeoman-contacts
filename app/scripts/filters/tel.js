813ae9240443a82ab4ce1c98ac0b16f68814221e
David Kim <daviddarden11@gmail.com>
2013-11-29 20:22:03 -0800

    tel.js updated

--- app/scripts/filters/tel.js
+++ app/scripts/filters/tel.js
@@ -3,8 +3,7 @@
 angular.module('yeomanContactsApp')
   .filter('tel', function () {
     return function (input) {
-      var pattern = /\b[1]?[(-|\(|\.|\s)]?([\d]{3})(-|\)|\.|\s)[\s]?
-              ([\d]{3})(-|\.|\s)([\d]{4})\b/g,
+      var pattern = /\b[1]?[(-|\(|\.|\s)]?([\d]{3})(-|\)|\.|\s)[\s]?([\d]{3})(-|\.|\s)([\d]{4})\b/g,
         replace = '($1)$3-$5';
       return input.replace(pattern, replace);
     };
